47d8ea989f882f29360682a7c5b711a5a1773343
melalex <melalex490@gmail.com>
2017-02-23 13:04:20 +0200

    Role entity

---
+++ src/main/java/com/room414/racingbets/dal/domain/entities/Role.java
@@ -0,0 +1,70 @@
+package com.room414.racingbets.dal.domain.entities;
+
+/**
+ * Class that represents user role.
+ *
+ * <h4>Fields</h4>
+ * <ul>
+ *      <li>id - role id</li>
+ *      <li>name - role name</li>
+ * </ul>
+ *
+ * @author Alexander Melashchenko
+ * @version 1.0 23 Feb 2017
+ */
+public class Role {
+    private int id;
+    private String name;
+
+    public Role() {
+
+    }
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Role role = (Role) o;
+
+        if (id != role.id) {
+            return false;
+        }
+
+        return name != null ? name.equals(role.name) : role.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+
+        return result;
+    }
+}
