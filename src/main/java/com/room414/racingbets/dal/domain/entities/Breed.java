6a842ab32506cfecfa86503262e38484a4056b30
melalex <melalex490@gmail.com>
2017-02-23 18:02:40 +0200

    SimpleEntity

---
+++ src/main/java/com/room414/racingbets/dal/domain/entities/Breed.java
@@ -0,0 +1,11 @@
+package com.room414.racingbets.dal.domain.entities;
+
+import java.io.Serializable;
+
+/**
+ * @author Alexander Melashchenko
+ * @version 1.0 23 Feb 2017
+ */
+public class Breed implements Serializable {
+
+}
