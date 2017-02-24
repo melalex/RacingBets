e4c35a1d463763810f77f4b88a86d75362005114
melalex <melalex490@gmail.com>
2017-02-24 16:49:13 +0200

    BetType entity

---
+++ src/main/java/com/room414/racingbets/dal/domain/entities/BetType.java
@@ -0,0 +1,28 @@
+package com.room414.racingbets.dal.domain.entities;
+
+import com.room414.racingbets.dal.abstraction.entities.SimpleEntity;
+
+/**
+ * Class that represents bet type.
+ *
+ * <h4>Fields</h4>
+ * <ul>
+ *      <li>id - bet type id in data storage</li>
+ *      <li>name - bet type name</li>
+ * </ul>
+ *
+ * @author Alexander Melashchenko
+ * @version 1.0 24 Feb 2017
+ */
+public class BetType extends SimpleEntity {
+    private static final long serialVersionUID = -8773086214148701974L;
+
+    @Override
+    public String toString() {
+        return "BetType{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+
+    }
+}
