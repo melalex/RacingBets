5337200663e313c7497a4ad63dda88fbfbe64112
melalex <melalex490@gmail.com>
2017-02-28 20:24:23 +0200

    UnitOfWorkFactory

---
+++ src/main/java/com/room414/racingbets/dal/abstraction/factories/UnitOfWorkFactory.java
@@ -0,0 +1,13 @@
+package com.room414.racingbets.dal.abstraction.factories;
+
+import com.room414.racingbets.dal.abstraction.dao.UnitOfWork;
+
+/**
+ * It manages the creation of UnitOfWork instances.
+ *
+ * @author Alexander Melashchenko
+ * @version 1.0 28 Feb 2017
+ */
+public interface UnitOfWorkFactory {
+    UnitOfWork create();
+}
