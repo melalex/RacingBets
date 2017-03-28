81806a96612d12508fabd7f728544e941a64ab50
melalex <melalex490@gmail.com>
2017-03-28 16:03:29 +0300

    routes.xml parse

--- src/main/java/com/room414/racingbets/web/servlet/LifeCycle.java
+++ src/main/java/com/room414/racingbets/web/servlet/LifeCycle.java
@@ -9,14 +9,19 @@ import com.room414.racingbets.web.controller.factories.ControllerFactoryCreator;
 import com.room414.racingbets.web.model.infrastructure.Route;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.servlet.ServletException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -25,6 +30,16 @@ import java.util.stream.Collectors;
  */
 class LifeCycle {
     private static final Log LOG = LogFactory.getLog(LifeCycle.class);
+
+    private static final String MYSQL_PROPERTIES = "mysql.properties";
+    private static final String REDIS_PROPERTIES = "redis.properties";
+    private static final String DAL_PROPERTIES = "dal.properties";
+
+    private static final String JWT_PROPERTIES = "jwt.properties";
+    private static final String MAIL_PROPERTIES = "mail.properties";
+    private static final String BLL_PROPERTIES = "bll.properties";
+
+    private static final String ROUTES = "routes.xml";
 
     private LifeCycle() {
 
@@ -43,17 +58,17 @@ class LifeCycle {
     }
 
     private static void initDal() throws ServletException {
-        Properties mysql = readProperties("mysql.properties");
-        Properties redis = readProperties("redis.properties");
-        Properties dal = readProperties("dal.properties");
+        Properties mysql = readProperties(MYSQL_PROPERTIES);
+        Properties redis = readProperties(REDIS_PROPERTIES);
+        Properties dal = readProperties(DAL_PROPERTIES);
 
         DalFacade.getInstance().initDal(mysql, redis, dal);
     }
 
     private static void initBll() throws ServletException {
-        Properties jwt = readProperties("jwt.properties");
-        Properties mail = readProperties("mail.properties");
-        Properties bll = readProperties("bll.properties");
+        Properties jwt = readProperties(JWT_PROPERTIES);
+        Properties mail = readProperties(MAIL_PROPERTIES);
+        Properties bll = readProperties(BLL_PROPERTIES);
 
         BllFacade.getInstance().init(mail, jwt, bll);
     }
@@ -75,8 +90,41 @@ class LifeCycle {
         return result;
     }
 
-    private static List<Route> getRoutes() {
-        return null;
+    private static List<Route> getRoutes() throws ServletException {
+        try (InputStream in = LifeCycle.class.getClassLoader().getResourceAsStream(ROUTES)) {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(in);
+            List<Route> result = new LinkedList<>();
+            NodeList nodeList = doc.getElementsByTagName("route");
+
+            Node node;
+            Element element;
+
+            Route.Method method;
+            String pattern;
+            String controller;
+            String action;
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    element = (Element) node;
+                    method = Route.Method.valueOf(element.getElementsByTagName("method").item(0).getTextContent());
+                    pattern = element.getElementsByTagName("method").item(0).getTextContent();
+                    controller = element.getElementsByTagName("controller").item(0).getTextContent();
+                    action = element.getElementsByTagName("action").item(0).getTextContent();
+
+                    result.add(new Route(method, pattern, controller, action));
+                }
+            }
+
+            return result;
+        } catch (IOException | SAXException | ParserConfigurationException e) {
+            String message = String.format("Exception during reading properties from file '%s'", ROUTES);
+            LOG.error(message, e);
+            throw new ServletException(message, e);
+        }
     }
 
     static void startUp(RacingBetsFrontController servlet) throws ServletException {
