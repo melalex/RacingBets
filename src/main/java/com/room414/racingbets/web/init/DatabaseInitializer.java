bb5c45c6a7850d46df9baaf4a87a3a1893094388
melalex <melalex490@gmail.com>
2017-04-05 01:50:59 +0300

    DatabaseInitializer

--- src/main/java/com/room414/racingbets/web/init/DatabaseInitializer.java
+++ src/main/java/com/room414/racingbets/web/init/DatabaseInitializer.java
@@ -1,6 +1,19 @@
 package com.room414.racingbets.web.init;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.room414.racingbets.bll.abstraction.services.*;
+import com.room414.racingbets.bll.concrete.facade.BllFacade;
+import com.room414.racingbets.bll.dto.entities.*;
 import com.room414.racingbets.dal.concrete.facade.DalFacade;
+import com.room414.racingbets.dal.domain.enums.Gender;
+import com.room414.racingbets.dal.domain.enums.Language;
+import com.room414.racingbets.dal.domain.enums.Role;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
 
 import static com.room414.racingbets.web.init.AppInitializer.initApp;
 
@@ -9,37 +22,182 @@ import static com.room414.racingbets.web.init.AppInitializer.initApp;
  * @version 1.0 04 Apr 2017
  */
 public class DatabaseInitializer {
-    private static void initUsers() {
+    private static final String HORSE_INITIAL_DATA = "initialData/Horse.json";
+    private static final String JOCKEY_INITIAL_DATA = "initialData/Jockey.json";
+    private static final String OWNER_INITIAL_DATA = "initialData/Owner.json";
+    private static final String RACECOURSE_INITIAL_DATA = "initialData/Racecourse.json";
+    private static final String TRAINER_INITIAL_DATA = "initialData/Trainer.json";
+    private static final String USER_INITIAL_DATA = "initialData/User.json";
 
+    private static void initSuperUser() {
+        UserDto superUser = new UserDto();
+        superUser.setLogin("melalex");
+        superUser.setPassword("secure");
+        superUser.setEmailConfirmed(true);
+        superUser.setEmail("melalex490@gmail.com");
+        superUser.setFirstName("Alexander");
+        superUser.setLastName("Melashchenko");
+        superUser.setLanguage(Language.RUSSIAN);
+        superUser.setBalance(BigDecimal.valueOf(1000));
+        superUser.addRole(Role.HANDICAPPER);
+        superUser.addRole(Role.ADMIN);
+        superUser.addRole(Role.BOOKMAKER);
+
+        BllFacade
+                .getInstance()
+                .getAbstractServiceFactory()
+                .createUserServiceFactory()
+                .create()
+                .create(superUser);
     }
 
-    private static void initHorses() {
+    private static void initUsers() throws IOException {
+        try (InputStream in = AppInitializer.class.getClassLoader().getResourceAsStream(USER_INITIAL_DATA)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            ObjectMapper jsonMapper = new ObjectMapper();
+            UserService userService = BllFacade
+                    .getInstance()
+                    .getAbstractServiceFactory()
+                    .createUserServiceFactory()
+                    .create();
 
+            UserDto userDto;
+            String line;
+
+            while (reader.ready()) {
+                line = reader.readLine();
+                userDto = jsonMapper.readValue(line, UserDto.class);
+
+                userService.create(userDto);
+            }
+        }
     }
 
-    private static void initJockeys() {
-
+    private static Gender getGender() {
+        long random = Math.round(Math.random() * 1);
+        return random == 0 ? Gender.STALLION : Gender.MARE;
     }
 
-    private static void initOwners() {
+    private static void initHorses() throws IOException {
+        try (InputStream in = AppInitializer.class.getClassLoader().getResourceAsStream(HORSE_INITIAL_DATA)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            ObjectMapper jsonMapper = new ObjectMapper();
+            HorseService horseService = BllFacade
+                    .getInstance()
+                    .getAbstractServiceFactory()
+                    .createHorseServiceFactory()
+                    .create();
 
+            HorseDto horseDto;
+            String line;
+
+            while (reader.ready()) {
+                line = reader.readLine();
+                horseDto = jsonMapper.readValue(line, HorseDto.class);
+
+                horseDto.setGender(getGender());
+
+                horseService.create(horseDto);
+            }
+        }
     }
 
-    private static void initRacecourses() {
+    private static void initJockeys() throws IOException {
+        try (InputStream in = AppInitializer.class.getClassLoader().getResourceAsStream(JOCKEY_INITIAL_DATA)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            ObjectMapper jsonMapper = new ObjectMapper();
+            JockeyService jockeyService = BllFacade
+                    .getInstance()
+                    .getAbstractServiceFactory()
+                    .createJockeyServiceFactory()
+                    .create();
 
+            JockeyDto jockeyDto;
+            String line;
+
+            while (reader.ready()) {
+                line = reader.readLine();
+                jockeyDto = jsonMapper.readValue(line, JockeyDto.class);
+
+                jockeyService.create(jockeyDto);
+            }
+        }
+    }
+
+    private static void initOwners() throws IOException {
+        try (InputStream in = AppInitializer.class.getClassLoader().getResourceAsStream(OWNER_INITIAL_DATA)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            ObjectMapper jsonMapper = new ObjectMapper();
+            OwnerService ownerService = BllFacade
+                    .getInstance()
+                    .getAbstractServiceFactory()
+                    .createOwnerServiceFactory()
+                    .create();
+
+            OwnerDto ownerDto;
+            String line;
+
+            while (reader.ready()) {
+                line = reader.readLine();
+                ownerDto = jsonMapper.readValue(line, OwnerDto.class);
+
+                ownerService.create(ownerDto);
+            }
+        }
+    }
+
+    private static void initRacecourses() throws IOException {
+        try (InputStream in = AppInitializer.class.getClassLoader().getResourceAsStream(RACECOURSE_INITIAL_DATA)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            ObjectMapper jsonMapper = new ObjectMapper();
+            RacecourseService racecourseService = BllFacade
+                    .getInstance()
+                    .getAbstractServiceFactory()
+                    .createRacecourseServiceFactory()
+                    .create();
+
+            RacecourseDto racecourseDto;
+            String line;
+
+            while (reader.ready()) {
+                line = reader.readLine();
+                racecourseDto = jsonMapper.readValue(line, RacecourseDto.class);
+
+                racecourseService.create(racecourseDto);
+            }
+        }
     }
 
     private static void initRaces() {
 
     }
 
-    private static void initTrainers() {
+    private static void initTrainers() throws IOException {
+        try (InputStream in = AppInitializer.class.getClassLoader().getResourceAsStream(TRAINER_INITIAL_DATA)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            ObjectMapper jsonMapper = new ObjectMapper();
+            TrainerService trainerService = BllFacade
+                    .getInstance()
+                    .getAbstractServiceFactory()
+                    .createTrainerServiceFactory()
+                    .create();
 
+            TrainerDto trainerDto;
+            String line;
+
+            while (reader.ready()) {
+                line = reader.readLine();
+                trainerDto = jsonMapper.readValue(line, TrainerDto.class);
+
+                trainerService.create(trainerDto);
+            }
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         initApp();
 
+        initSuperUser();
         initUsers();
         initHorses();
         initJockeys();
